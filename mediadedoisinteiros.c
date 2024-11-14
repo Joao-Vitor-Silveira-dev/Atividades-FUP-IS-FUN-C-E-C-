e73041ff2661998647c4cdea6bce470249a3695f
João Vitor Silveira <80629536+Joao-Vitor-Silveira-dev@users.noreply.github.com>
2024-11-14 14:22:40 -0300

    Add files via upload

---
+++ mediadedoisinteiros.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+    int main(){
+        float numero1,numero2;//CRIACAO DE 2 VAR FLOAT
+            scanf("%f%f",&numero1,&numero2);// LEITURA 2 NUMEROS
+                printf("%.1f\n",(numero1+numero2)/2);//IMPRIME MEDIA
+//CODE BY JVS_MAD_HATTER *_*
+return(0);}
