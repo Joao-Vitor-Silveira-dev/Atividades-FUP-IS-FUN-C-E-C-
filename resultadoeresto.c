bde44ed6c6050ae604576344daacc35cec440aad
João Vitor Silveira <80629536+Joao-Vitor-Silveira-dev@users.noreply.github.com>
2024-11-14 13:57:12 -0300

    Add files via upload

---
+++ resultadoeresto.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+    int main(){
+        int numero1,numero2;//Criacao de 2 variaveis
+            scanf("%d%d",&numero1,&numero2);//Leitura de 2 variaveis inteiras
+                printf("%d %d\n",(numero1/numero2),(numero1%numero2));//Retorno de quociente e resto
+//CODE BY JVS_MAD_HATTER
+return(0);}
